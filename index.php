f533d8d43266cb2ce07ea4a643efcda1a81a58f5
sugiura <sugiura@winas.jp>
2023-12-08 09:21:27 +0900

    問い合わせフォーム

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 
 echo "Hey World!";
+echo "<form action='submit.php' method='post'>";
+echo "<label for='name'>名前:</label><br>";
+echo "<input type='text' id='name' name='name'><br>";
+echo "<label for='email'>メール:</label><br>";
+echo "<input type='text' id='email' name='email'><br>";
+echo "<input type='submit' value='送信'>";
+echo "</form>";
